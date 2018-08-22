a0f4d6d57cf40252564b6531df0ab207c640f278
brianngeno <bkn.ngeno@gmail.com>
2018-08-22 07:08:57 +0300

    moved apiUrl

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,9 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'https://api.github.com/users/BrianNgeno?access_token=2b123e7e188464c6df4b5cc9e1b95747706d4bc1',
-  apiUrl1: 'https://api.github.com/users/BrianNgeno/repos?access_token=2b123e7e188464c6df4b5cc9e1b95747706d4bc1',
-  apiUrl2: 'https://api.github.com/search/users?q='
+
 };
 
 /*
